1ea4efab5b76b0880fc3dcca07824ee66f95d0ba
Mark <marknoviko@mail.ru>
2023-09-17 13:32:02 +0300

    tc

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            test
+        </div>
+    );
 }
 
 export default App;
